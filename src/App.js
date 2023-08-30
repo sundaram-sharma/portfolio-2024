7543ee28d91d0b4b79da2db0c311cd04eca4aebc
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-08-30 20:36:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home/Homescreen";
+import HeroSection from "./Pages/Home/HeroSection";
+import AboutMe from "./Pages/Home/AboutMe";
+import MyPortfolio from "./Pages/Home/MyPortfolio";
+import ContactMe from "./Pages/Home/ContactMe";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/main" element={<HeroSection />}></Route>
+            <Route path="/about" element={<AboutMe />}></Route>
+            <Route path="/portfolio" element={<MyPortfolio />}></Route>
+            {/* <Route path="/blog" element={<blog />}></Route> */}
+            <Route path="/contact" element={<ContactMe />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,23 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Sundaram</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+              <h4>Let's turn ideas into reality together</h4>
+          </p>
+        </div>
+        <button className="btn btn-primary">Contact Me</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
